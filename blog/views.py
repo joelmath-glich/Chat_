e8da34b42c2492f4120ca0b5a074b6091d34c664
joelmath-glich <joelmath19@gmail.com>
2025-04-08 22:16:56 +0530

    test

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 # Create your views here.
+
+
 def index(request):
     return render(request,"blog/index.html")
 
 def specific(request):
     return HttpResponse('this is specific')
+
+def getResponse(request):
+    
